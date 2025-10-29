ded8114f07ddad00bca3e651fae7ea05ef3d80fb
dobhalsaurish <saurishdobhal@gmail.com>
2025-10-29 21:53:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const SUPABASE_CONFIG = {
+  url: 'https://aenikubkzquqjbnwiojn.supabase.co', // e.g., https://xxxxx.supabase.co
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbmlrdWJrenF1cWpibndpb2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NzQzOTUsImV4cCI6MjA3NzI1MDM5NX0.ymt7BEAdA9mjd4XERYGiRhJzzdmRbGpZXIROXM_bBzo'
+};
+
+const OMDB_API_KEY = 'd368ac0e'; // Replace with your own key
